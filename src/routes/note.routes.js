73649267a00c6e97a127b3750008370da3f1c1c0
Kumar-Aditya-DEV <aditya.kumar.j.cg@gmail.com>
2026-04-17 17:52:55 +0530

    feat: add fetch single note endpoint (GET /api/notes/:id)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes, getAllNotes } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes, getNoteById } = require("../controllers/note.controller");
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
 
